594b0395851b90f291ed420e204993c20aad3352
Reetak Rana <reetakrana65@gmail.com>
2025-07-18 23:15:21 +0530

    Update 17 july assignment.py

--- 17 july assignment.py
+++ 17 july assignment.py
@@ -1,18 +1,23 @@
+# Q1 Create a function greet(name) that prints “Hello, [name]!” .
 def greeting(name):
     print("Hello, " + name )
 greeting("REETAK RANA")
 
+#Q2. Create a function add(a, b) that returns the sum of two numbers. Call the function with inputs.
 def add(a,b):
    a=int(input("Enter first number: "))
    b=int(input("Enter second number: "))
    return a + b
-print("The sum is:", add(a,b))  # Call the function to see the result  
+print("The sum is:", add(a,b))  
 
 
+#Q3. Create a function display(*args) that accepts multiple arguments and prints them.
 def delta(*jatin):
         print(f"my name is {jatin}")
 delta("Reetak","Rana")
 
+
+#Q4. Create a function student_info(**kwargs) that accepts name, age, roll number as keyword arguments and prints them.
 def delta(**jatin):
         print(f"my name is {jatin}")
 delta("Reetak":"Rana")
